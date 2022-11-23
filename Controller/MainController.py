a8fbdc7efe598071d0fce5707b36a3ef421daa62
zayaanm <zayaan.momin2@gmail.com>
2022-11-23 12:43:17 -0500

    Main Controller edit

---
+++ Controller/MainController.py
@@ -0,0 +1,23 @@
+from Model.MainModel import MainModel
+from View.MainView import MainView
+from Controller.LeagueController import LeagueController
+
+
+class MainController:
+
+    def __init__(self):
+        self.controller = self
+
+    def printMain():
+
+        print('League stats')
+
+    def printLeague():
+        LeagueController.method()
+        print('League stats')
+
+    def printPlayer():
+        print('League stats')
+
+    def printNews():
+        print('League stats')
