a230734bc801542d87d73f188fd91f8e76286172
zayaanm <zayaan.momin2@gmail.com>
2022-11-27 07:05:40 -0500

    Added Api lookup and display for league confernces

--- Controller/LeagueController.py
+++ Controller/LeagueController.py
@@ -1,20 +1,36 @@
-#from Model.LeagueModel import LeagueModel
 import sys, os
-sys.path.append(os.path.abspath(".."))
+parentddir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+sys.path.append(parentddir)
 from View.LeagueView import LeagueView
+from Model.LeagueModel import LeagueModel
 
 class LeagueController:
 
     def __init__(self):
-        self.LeagueView = LeagueView
+        self.LeagueView = LeagueView()
+        self.LeagueModel = LeagueModel()
         
+    def menuPrompt(self):
+       return self.LeagueView.displayMainMenu()
 
-    def prompt(self):
-       self.LeagueView.displayMainMenu()
+    def getData(self, dataOption):
+        if dataOption == '1':
+            self.LeagueModel.findPlayerStats()
+            self.LeagueView.displayPlayerInfo(self.LeagueModel.getPlayerStats())
+
+        elif dataOption == '2':
+            year, conference = self.LeagueView.displayConferenceInfoPrompt()
+            self.LeagueModel.findConferenceStats(year, conference)
+            self.LeagueView.displayConferenceInfo(self.LeagueModel.getConferenceStats())
+
+        else:
+            print('err')
+
 
 def main():
     league = LeagueController()
-    league.prompt()
+    option = league.menuPrompt()
+    league.getData(option)
 
 if __name__ == "__main__":
     main()
