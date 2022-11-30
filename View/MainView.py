fa2d22cc1fcd0347980149711e63f3e51c2d21c6
Leon Nasehi <lnasehi@uwo.ca>
2022-11-30 18:21:31 -0500

    View/MainView.py

--- View/MainView.py
+++ View/MainView.py
@@ -7,7 +7,7 @@ class MainView:
         option = input('->')
 
         while option != '1' and option != '0':
-            print("Select an option by choosing a number:\n0. Exit program \n1. League Controller\n2. Search For A Team To See The Active Roster")
+            print('Invalid selection. Please select an option choosing a number:\n0. Exit program \n1. League Controller\n2. Search For A Team To See The Active Roster')
             option = input('->')
 
         return option
