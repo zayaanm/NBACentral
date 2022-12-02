467696cf4c9e954502e1a36eeb4668907ad3d4b5
MuhammadKhan-stack <mkhan884@uwo.ca>
2022-12-02 18:39:28 -0500

    adding tables to news, making sure news api works

--- Model/NewsModel.py
+++ Model/NewsModel.py
@@ -1,5 +1,7 @@
 import sys, os
 import json
+from prettytable import PrettyTable
+
 parentddir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(parentddir)
 
@@ -10,18 +12,37 @@ class NewsModel:
         self.teamNews = []
 
     def pullAllNews(self):
+        allNewsTable = PrettyTable()
+        allNewsTable.field_names = ["Title", "URL", "Source"]
+        allNewsTable.align = "l"
+        allNewsTable.max_width= 100
+
         allNews = self.ApiCaller.newsApiCall('https://nba-latest-news.p.rapidapi.com/news')
-        allNews = json.loads(allNews)
-        self.allNews = allNews
-    
+        dictionary = json.loads(allNews)
+
+        for news in dictionary:
+            info = news['title'], news['url'], news['source']
+            allNewsTable.add_row(info)
+
+        self.allNews = allNewsTable
+
     def getAllNews(self):
         return self.allNews
 
     def pullTeamNews(self, teamName):
+        teamNewsTable = PrettyTable()
+        teamNewsTable.field_names = ["Title", "URL", "Source"]
+        teamNewsTable.align = "l"
+        teamNewsTable.max_width= 100
+        
         teamNews = self.ApiCaller.newsApiCall(f"https://nba-latest-news.p.rapidapi.com/news/team/{teamName}")
-        teamNews = json.loads(teamNews)
-        # print(teamNews)
-        self.teamNews = teamNews
+        dictionary = json.loads(teamNews)
+
+        for news in dictionary:
+            info = news['title'], news['url'], news['source']
+            teamNewsTable.add_row(info)
+
+        self.teamNews = teamNewsTable
 
     def getTeamNews(self):
         return self.teamNews
